b557c64121f268681b8bdddb4f4b1a497c981bcc
Olof Sjöholm <olof.sjoholm@walmartlabs.com>
2021-10-20 04:14:42 +0200

    Build

---
+++ tank/map/board.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="board" tilewidth="75" tileheight="75" tilecount="12" columns="12">
+ <image source="board.png" width="900" height="75"/>
+</tileset>
---
+++ tank/map/conveyer_and_misc.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="conveyer_and_misc" tilewidth="75" tileheight="75" tilecount="35" columns="7">
+ <image source="conveyer_and_misc.png" width="525" height="375"/>
+</tileset>
